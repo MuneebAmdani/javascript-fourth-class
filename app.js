8c5a2d4dd4ea0c0b3729bb4dbe80c64701c6da14
MuneebAmdani <arhammuneeb239@gmail.com>
2024-11-14 12:52:51 +0500

    javascript fourth class

---
+++ app.js
@@ -0,0 +1,37 @@
+// let name1='zubair';
+// if(name1!="zubair"){
+//     alert("yes")
+// }
+
+// let name1='2';
+// if(name1===2){
+//     alert("yes")
+// }
+
+// let name1='2';
+// if(name1===2){
+//     alert("yes")
+// }
+
+// let name1='2';
+// if(name1!=2){
+//     alert("yes")
+// }
+
+// let name1='2';
+// if(name1!=2){
+//     alert("yes")
+// }
+
+// if('2'==2){
+//     alert("yes")
+// }
+
+// if('2'!=2){
+//     alert("yes")
+// }
+
+// if('2'!==2){
+//     alert("yes")
+// }
+
